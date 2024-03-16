ca160bb3739fc7ba020da26caa959f8ca66e1b03
Agustin Gallego <77341002+gallegoagustin@users.noreply.github.com>
2024-03-16 18:51:25 +0000

    migrated activities arch

--- src/components/modules/activities/activities.container.tsx
+++ src/components/modules/activities/activities.container.tsx
@@ -1,6 +1,150 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import useDynamicTranslation from '@/components/hooks/useDynamicTranslation';
+import { useRouter } from 'next/router';
 import Activities from './activities';
 
+import ImageCard1 from '@public/images/icons/isotipos/onu.svg';
+import ImageCard2 from '@public/images/icons/isotipos/oea.svg';
+import ImageCard3 from '@public/images/icons/isotipos/rastros.svg';
+import ImageCard4 from '@public/images/icons/isotipos/g20.svg';
+import ImageCard5 from '@public/images/icons/isotipos/electoral.svg';
+import ImageCard6 from '@public/images/icons/isotipos/sena.svg';
+import ImageCard7 from '@public/images/icons/isotipos/hcd.svg';
+
 export default function ActivitiesContainer() {
-  return <Activities />;
+  const router = useRouter();
+  const activitiesContainerRef = useRef(null);
+  const { i18nActivities } = useDynamicTranslation();
+
+  const activitiesCards = [
+    {
+      title: i18nActivities('activities.onu.title', { plainText: true }),
+      subtitle: i18nActivities('activities.onu.subtitle', { plainText: true }),
+      participants: i18nActivities('activities.onu.participants', {
+        plainText: true,
+      }),
+      image: ImageCard1,
+      link_utils:
+        'https://drive.google.com/drive/folders/1i6XlJmrZ4LwAYxeaAEVcv1t_yFRIT9Jm?usp=drive_link',
+      date: i18nActivities('activities.onu.date', { plainText: true }),
+      date2: i18nActivities('activities.onu.date2', { plainText: true }),
+    },
+    {
+      title: i18nActivities('activities.un.title', { plainText: true }),
+      subtitle: i18nActivities('activities.un.subtitle', { plainText: true }),
+      participants: i18nActivities('activities.un.participants', {
+        plainText: true,
+      }),
+      image: ImageCard1,
+      date: i18nActivities('activities.un.date', { plainText: true }),
+      link_utils:
+        'https://drive.google.com/drive/folders/1PoSp8yJhVkY9Iqg-sEN323XJLPdTcT9O',
+    },
+    {
+      title: i18nActivities('activities.oea.title', { plainText: true }),
+      subtitle: i18nActivities('activities.oea.subtitle', { plainText: true }),
+      participants: i18nActivities('activities.oea.participants', {
+        plainText: true,
+      }),
+      image: ImageCard2,
+      link_utils:
+        'https://drive.google.com/drive/folders/1crxRST6eTpo68h0Fux8VHIuqkhRiYxc2?usp=drive_link',
+      date: i18nActivities('activities.oea.date', { plainText: true }),
+    },
+    {
+      title: i18nActivities('activities.rastros.title', { plainText: true }),
+      subtitle: i18nActivities('activities.rastros.subtitle', {
+        plainText: true,
+      }),
+      participants: i18nActivities('activities.rastros.participants', {
+        plainText: true,
+      }),
+      image: ImageCard3,
+      link_utils:
+        'https://drive.google.com/drive/folders/1fO-pKsjNsHOk_-Jw9MNPzBsKAmu34EBi?usp=drive_link',
+      date: i18nActivities('activities.rastros.date', { plainText: true }),
+    },
+    {
+      title: i18nActivities('activities.hcd.title', { plainText: true }),
+      subtitle: i18nActivities('activities.hcd.subtitle', { plainText: true }),
+      participants: i18nActivities('activities.hcd.participants', {
+        plainText: true,
+      }),
+      image: ImageCard7,
+      date: i18nActivities('activities.hcd.date', { plainText: true }),
+      link_utils:
+        'https://drive.google.com/drive/folders/1oE_XR7_Pt1HOLZYvE1njqxoQCNbN_F7K?usp=drive_link',
+    },
+    {
+      title: i18nActivities('activities.sena.title', { plainText: true }),
+      subtitle: i18nActivities('activities.sena.subtitle', { plainText: true }),
+      participants: i18nActivities('activities.sena.participants', {
+        plainText: true,
+      }),
+      image: ImageCard6,
+      date: i18nActivities('activities.sena.date', { plainText: true }),
+      link_utils:
+        'https://drive.google.com/drive/folders/1wHK6uI7TSphJT3o8hgtOb73ihU1AFTms',
+    },
+    {
+      title: i18nActivities('activities.g20.title', { plainText: true }),
+      subtitle: i18nActivities('activities.g20.subtitle', { plainText: true }),
+      participants: i18nActivities('activities.g20.participants', {
+        plainText: true,
+      }),
+      image: ImageCard4,
+      date: i18nActivities('activities.g20.date', { plainText: true }),
+      link_utils:
+        'https://drive.google.com/drive/folders/1YCVeJzKToTeQVuExDOtp_rlidTC7rZIC',
+    },
+    {
+      title: i18nActivities('activities.electoral.title', { plainText: true }),
+      subtitle: i18nActivities('activities.electoral.subtitle', {
+        plainText: true,
+      }),
+      participants: i18nActivities('activities.electoral.participants', {
+        plainText: true,
+      }),
+      image: ImageCard5,
+      date: i18nActivities('activities.electoral.date', { plainText: true }),
+      link_utils:
+        'https://drive.google.com/drive/folders/1DcoGqkA0XmZZhtul-WMiv-m_uIZvJ0FL',
+    },
+  ];
+
+  useEffect(() => {
+    const handleScrollToElement = () => {
+      // Obtén el fragmento de la URL
+      const fragment = router.asPath.split('#')[1];
+
+      if (fragment) {
+        // Verifica si el elemento container ya está en el DOM
+        if (activitiesContainerRef.current) {
+          // Encuentra el elemento con el ID correspondiente al fragmento
+          const element = document.getElementById(fragment);
+
+          if (element) {
+            // Calcula la posición del elemento en la página
+            const elementPosition = element.getBoundingClientRect().top;
+
+            // Realiza el desplazamiento suave hacia el elemento
+            window.scrollTo({
+              top: elementPosition,
+              behavior: 'smooth',
+            });
+          }
+        }
+      }
+    };
+
+    // Llama a la función de manejo al cargar la página
+    handleScrollToElement();
+  }, [router.asPath]);
+
+  const childProps = {
+    activitiesCards,
+    activitiesContainerRef,
+  };
+
+  return <Activities {...childProps} />;
 }
--- src/components/modules/activities/activities.tsx
+++ src/components/modules/activities/activities.tsx
@@ -1,125 +1,28 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import useDynamicTranslation from '@/components/hooks/useDynamicTranslation';
 import LazyLoad from 'react-lazyload';
-import ImageCard1 from '@public/images/icons/isotipos/onu.svg';
-import ImageCard2 from '@public/images/icons/isotipos/oea.svg';
-import ImageCard3 from '@public/images/icons/isotipos/rastros.svg';
-import ImageCard4 from '@public/images/icons/isotipos/g20.svg';
-import ImageCard5 from '@public/images/icons/isotipos/electoral.svg';
-import ImageCard6 from '@public/images/icons/isotipos/sena.svg';
-import ImageCard7 from '@public/images/icons/isotipos/hcd.svg';
 
-export default function Activities() {
-  const activitiesCards = [
-    {
-      title: 'onu',
-      subtitle: 'Simulacro de la Organización de las Naciones Unidas',
-      participants: '4º, 5º y 6º año.',
-      image: ImageCard1,
-      link_utils:
-        'https://drive.google.com/drive/folders/1i6XlJmrZ4LwAYxeaAEVcv1t_yFRIT9Jm?usp=drive_link',
-      date: 'Fecha estimada primera edición: mes de septiembre',
-      date2: 'Fecha estimada segunda edición: mes de octubre',
-    },
-    {
-      title: 'un',
-      subtitle:
-        'Simulacro de la Organización de las Naciones Unidas (en inglés)',
-      participants: '4º, 5º y 6º año.',
-      image: ImageCard1,
-      date: 'Fecha estimada: mes de junio',
-      link_utils:
-        'https://drive.google.com/drive/folders/1PoSp8yJhVkY9Iqg-sEN323XJLPdTcT9O',
-    },
-    {
-      title: 'oea',
-      subtitle: 'Simulacro de la Organización de Estados Americanos',
-      participants: '1º, 2º y 3º año.',
-      image: ImageCard2,
-      link_utils:
-        'https://drive.google.com/drive/folders/1crxRST6eTpo68h0Fux8VHIuqkhRiYxc2?usp=drive_link',
-      date: 'Fecha estimada: mes de septiembre',
-    },
-    {
-      title: 'rastros',
-      subtitle: 'Simulacro de la Investigación de un caso Forense',
-      participants: '1º, 2º y 3º año.',
-      image: ImageCard3,
-      link_utils:
-        'https://drive.google.com/drive/folders/1fO-pKsjNsHOk_-Jw9MNPzBsKAmu34EBi?usp=drive_link',
-      date: 'Fecha estimada: mes de agosto',
-    },
-    {
-      title: 'hcd',
-      subtitle: 'Simulacro del Honorable Concejo Deliberante',
-      participants: '4º, 5º y 6º año.',
-      image: ImageCard7,
-      date: 'Fecha estimada: mes de julio',
-      link_utils:
-        'https://drive.google.com/drive/folders/1oE_XR7_Pt1HOLZYvE1njqxoQCNbN_F7K?usp=drive_link',
-    },
-    {
-      title: 'sena',
-      subtitle: 'Simulacro del Senado de la Nación',
-      participants: '4º, 5º y 6º año.',
-      image: ImageCard6,
-      date: 'Fecha estimada: mes de octubre',
-      link_utils:
-        'https://drive.google.com/drive/folders/1wHK6uI7TSphJT3o8hgtOb73ihU1AFTms',
-    },
-    {
-      title: 'g20',
-      subtitle: 'Simulacro del Grupo de los 20',
-      participants: '4º, 5º y 6º año.',
-      image: ImageCard4,
-      date: 'Próxima edición 2025',
-      link_utils:
-        'https://drive.google.com/drive/folders/1YCVeJzKToTeQVuExDOtp_rlidTC7rZIC',
-    },
-    {
-      title: 'electoral',
-      subtitle: 'Simulacro de Elecciones',
-      participants: '4º, 5º y 6º año.',
-      image: ImageCard5,
-      date: 'Próxima edición 2027',
-      link_utils:
-        'https://drive.google.com/drive/folders/1DcoGqkA0XmZZhtul-WMiv-m_uIZvJ0FL',
-    },
-  ];
+interface Props {
+  activitiesContainerRef: React.RefObject<HTMLDivElement>;
+  activitiesCards: {
+    title: string;
+    subtitle: string;
+    participants: string;
+    image: StaticImport;
+    link_utils: string;
+    date: string;
+    date2?: string;
+  }[];
+}
 
-  const router = useRouter();
-  const activitiesContainerRef = useRef(null);
-
-  useEffect(() => {
-    const handleScrollToElement = () => {
-      // Obtén el fragmento de la URL
-      const fragment = router.asPath.split('#')[1];
-
-      if (fragment) {
-        // Verifica si el elemento container ya está en el DOM
-        if (activitiesContainerRef.current) {
-          // Encuentra el elemento con el ID correspondiente al fragmento
-          const element = document.getElementById(fragment);
-
-          if (element) {
-            // Calcula la posición del elemento en la página
-            const elementPosition = element.getBoundingClientRect().top;
-
-            // Realiza el desplazamiento suave hacia el elemento
-            window.scrollTo({
-              top: elementPosition,
-              behavior: 'smooth',
-            });
-          }
-        }
-      }
-    };
-
-    // Llama a la función de manejo al cargar la página
-    handleScrollToElement();
-  }, [router.asPath]);
+export default function Activities({
+  activitiesContainerRef,
+  activitiesCards,
+}: Props) {
+  const { i18nActivities } = useDynamicTranslation();
 
   return (
     <section
@@ -140,7 +43,7 @@ export default function Activities() {
           }}
         >
           <h1 className={'text-WHITE text-4xl lg:text-5xl text-center'}>
-            Nuestras Actividades
+            {i18nActivities('header')}
           </h1>
         </div>
       </LazyLoad>
@@ -212,7 +115,7 @@ export default function Activities() {
                     id="text-container"
                   >
                     <p className={'font-bold my-6 text-lg'}>{item.subtitle}</p>
-                    <h2 className={'font-bold'}>¿Quiénes pueden participar?</h2>
+                    <h2 className={'font-bold'}>{i18nActivities('for')}</h2>
                     <p className={'my-2'}>{item.participants}</p>
                   </div>
                   <div className="text-center col gap-2">
@@ -226,7 +129,7 @@ export default function Activities() {
                     target="_blank"
                     className={'w-fit'}
                   >
-                    <button>Más información</button>
+                    <button>{i18nActivities('info')}</button>
                   </Link>
                 </div>
               </div>
