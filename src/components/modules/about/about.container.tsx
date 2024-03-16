9c356be4c78e54db386dda5aa1f243f19c3afce6
Agustin Gallego <77341002+gallegoagustin@users.noreply.github.com>
2024-03-16 19:07:52 +0000

    migrated about arch

--- src/components/modules/about/about.container.tsx
+++ src/components/modules/about/about.container.tsx
@@ -1,6 +1,72 @@
 import React from 'react';
+import useDynamicTranslation from '@/components/hooks/useDynamicTranslation';
 import About from './about';
 
 export default function AboutContainer() {
-  return <About />;
+  const { i18nAbout } = useDynamicTranslation();
+
+  const autorities = [
+    {
+      name: 'Alan Ezequiel Castillo',
+      age: '21',
+      position: i18nAbout('authorities.one.position', { plainText: true }),
+      info: i18nAbout('authorities.one.text', { plainText: true }),
+      image:
+        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013141/nexxos/alan_ar2npf.jpg?_s=public-apps',
+    },
+    {
+      name: 'Melissa Ayelen Oliva',
+      age: '20',
+      position: i18nAbout('authorities.two.position', { plainText: true }),
+      info: i18nAbout('authorities.two.text', { plainText: true }),
+      image:
+        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013140/nexxos/melisaayelen_fvtvp4.jpg?_s=public-apps',
+    },
+    {
+      name: 'Zoe Holgado',
+      age: '21',
+      position: i18nAbout('authorities.three.position', { plainText: true }),
+      info: i18nAbout('authorities.three.text', { plainText: true }),
+      image:
+        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013140/nexxos/zoe_ut1md5.jpg?_s=public-apps',
+    },
+    {
+      name: 'Valentina Fardin',
+      age: '23',
+      position: i18nAbout('authorities.four.position', { plainText: true }),
+      info: i18nAbout('authorities.four.text', { plainText: true }),
+      image:
+        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013142/nexxos/valentina_hnewix.jpg?_s=public-apps',
+    },
+    {
+      name: 'Sofía Ali',
+      age: '20',
+      position: i18nAbout('authorities.five.position', { plainText: true }),
+      info: i18nAbout('authorities.five.text', { plainText: true }),
+      image:
+        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013140/nexxos/sofia_jai2fi.jpg?_s=public-apps',
+    },
+    {
+      name: 'Maitena Granda',
+      age: '19',
+      position: i18nAbout('authorities.six.position', { plainText: true }),
+      info: i18nAbout('authorities.six.text', { plainText: true }),
+      image:
+        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013142/nexxos/maitena_q4dnbz.jpg?_s=public-apps',
+    },
+    {
+      name: 'Emiliano Iché',
+      age: '20',
+      position: i18nAbout('authorities.seven.position', { plainText: true }),
+      info: i18nAbout('authorities.seven.text', { plainText: true }),
+      image:
+        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013141/nexxos/emiliano_nrwys5.jpg?_s=public-apps',
+    },
+  ];
+
+  const childProps = {
+    autorities,
+  };
+
+  return <About {...childProps} />;
 }
--- src/components/modules/about/about.tsx
+++ src/components/modules/about/about.tsx
@@ -8,66 +8,20 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import useDynamicTranslation from '@/components/hooks/useDynamicTranslation';
 
-export default function About() {
-  const autorities = [
-    {
-      name: 'Alan Ezequiel Castillo',
-      age: '21',
-      position: 'Presidente',
-      info: '"Estoy en tercer año del profesorado en Matemática y con el tiempo que llevo siendo voluntario en la ONG, descubrí muchísimas herramientas que, actualmente, hacen que siga formándome como docente. Sin embargo, estoy convencido que la educación secundaria necesita un cambio. Ese cambio lo encontré en NEXXOS y es donde quiero seguir apostando para lograr que los jóvenes se desafíen y desarrollen habilidades ¿Como se logra eso? Construyendo una educación integral y de calidad que permita a los y las estudiantes a potenciarse en cada aspecto de su vida. Seamos parte de la transformación del presente garantizando los frutos para el futuro."',
-      image:
-        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013141/nexxos/alan_ar2npf.jpg?_s=public-apps',
-    },
-    {
-      name: 'Melissa Ayelen Oliva',
-      age: '20',
-      position: 'Vicepresidente',
-      info: '"Soy estudiante de Derecho en la UNMDP. En mi recorido por la secundaria participe en Modelos de Naciones Unidas realizados por Nexxos y gracias a esas experiencias pude descubrir que a lo que me queria dedicar era a las Relaciones Internacionales. Participe de Simulacros a nivel nacional e internacional junto a la Facultad. En 2022 me uni a la ONG con el objetivo de adentrarme en la organización que tiene cada uno de los simulacros y sobre todo para poder cambiar y mejorar constantemente la educación. Nexxos es un espacio en el que prevalece el respeto, la perseverancia y el aprender a romper con muchas de las estructuras impuestas por la sociedad y uno mismo."',
-      image:
-        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013140/nexxos/melisaayelen_fvtvp4.jpg?_s=public-apps',
-    },
-    {
-      name: 'Zoe Holgado',
-      age: '21',
-      position: 'Secretaria General',
-      info: '"Estudiante de la Licenciatura en Administración de Empresas. Conocí NEXXOS cuando estaba en el secundario, y me uní para devolver al menos un poco de lo que la organización hizo por mí cuando era participante. Desde entonces, he tenido la oportunidad de integrar diferentes proyectos y actividades en lo que respecta a la educación, ello me ha permitido adquirir experiencia en gestión de proyectos, liderazgo y sobre todas las cosas trabajo en equipo. Además, he podido conocer acerca de las realidades y problemáticas que aquejan en la educación actual, y estoy totalmente convencida que hay que hacer un cambio, y es por eso hacemos lo que hacemos."',
-      image:
-        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013140/nexxos/zoe_ut1md5.jpg?_s=public-apps',
-    },
-    {
-      name: 'Valentina Fardin',
-      age: '23',
-      position: 'Tesorera',
-      info: '"Estudiante de Licenciatura y Profesorado en Geografía. Mi pasión por la educación y mi convicción de empoderar a los jóvenes me llevaron a unirme a NEXXOS.  Cómo estudiante comprendo la importancia de una educación que desarrolle habilidades poderosas que nos permitan enfrentar los desafíos del mundo actual. En NEXXOS encontré la plataforma perfecta para fomentar un pensamiento crítico de la realidad y a alzar sus voces para construir un futuro más brillante."',
-      image:
-        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013142/nexxos/valentina_hnewix.jpg?_s=public-apps',
-    },
-    {
-      name: 'Sofía Ali',
-      age: '20',
-      position: 'Vocal',
-      info: '"Unirme a Nexxos fue el primer paso para contribuir en el cambio de lo que más influyó en quién soy hoy, la educación. Ser voluntaria me permite aprender constantemente de quienes me rodean y el trabajo en equipo me motiva a sacar cada día una mejor versión de mi. Estudio derecho en la UNMDP, represente a mi facultad en modelos de ONU realizados en el país y en el exterior. Me interesa el feminismo y en los fines de semana suelo dar apoyo escolar a jovenes y niños/as de manera voluntaria."',
-      image:
-        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013140/nexxos/sofia_jai2fi.jpg?_s=public-apps',
-    },
-    {
-      name: 'Maitena Granda',
-      age: '19',
-      position: 'Vocal',
-      info: '"Son pocas las cosas que tengo en claro y que no necesito reafirmar continuamente, una de ellas es mi pertenencia en NEXXOS, la disconformidad que en un principio me condujo hasta el voluntariado se transformó en interés por la educación y ayudar a jóvenes a desarrollar pensamiento crítico. Además de ayudar a formar ciudadanos íntegros en esta ong, estoy cursando el segundo año de la carrera de Comunicación Audiovisual en la UNMDP."',
-      image:
-        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013142/nexxos/maitena_q4dnbz.jpg?_s=public-apps',
-    },
-    {
-      name: 'Emiliano Iché',
-      age: '20',
-      position: 'Vocal',
-      info: '"Soy estudiante de historia, y es por ello que encontré en NEXXOS la posibilidad de trabajar con jóvenes, con quiénes son el futuro, sin estar graduado. Sin embargo, entiendo la educación secundaria necesita un cambio estructural muy profundo, y veo en la ONG los pilares de esa modificación, los valores y las herramientas que se brindan desde otra forma de enseñar son la base de una mejor educación secundaria."',
-      image:
-        'https://res.cloudinary.com/gallegoagustin/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710013141/nexxos/emiliano_nrwys5.jpg?_s=public-apps',
-    },
-  ];
+interface Props {
+  autorities: {
+    name: string;
+    age: string;
+    position: string;
+    info: string;
+    image: string;
+  }[];
+}
+
+export default function About({ autorities }: Props) {
+  const { i18nAbout } = useDynamicTranslation();
 
   return (
     <section className={'full col justify-start items-center'}>
@@ -85,7 +39,7 @@ export default function About() {
           }}
         >
           <h1 className={'text-WHITE text-4xl lg:text-5xl text-center'}>
-            Conocenos
+            {i18nAbout('header')}
           </h1>
         </div>
       </LazyLoad>
@@ -166,18 +120,11 @@ export default function About() {
       >
         <div className={'w-5/6 md:w-1/3'}>
           <p className={'my-6 font-montserrat text-3xl'}>
-            ¡Sumate como voluntario!
+            {i18nAbout('volunteerHeader')}
           </p>
-          <h1 className={'mb-6'}>Sé parte de la transformación educativa.</h1>
+          <h1 className={'mb-6'}>{i18nAbout('volunteerSubtitle')}</h1>
           <p className={'leading-relaxed text-base'}>
-            Si terminaste el colegio secundario.
-            <br /> Si sos mayor de edad.
-            <br /> Si te gusta explorar en las nuevas y diferentes maneras de
-            aprender.
-            <br /> Si te interesan las actividades académicas.
-            <br /> Y sobre todo, si tenés ganas de contribuir a innovar en
-            educación…
-            <br /> ¡Sumate a NEXXOS!
+            {i18nAbout('volunteerText')}
           </p>
         </div>
         <div
@@ -203,7 +150,7 @@ export default function About() {
                 'main-red-button mt-8 transition duration-300 hover:bg-RED_G20 w-[300px]'
               }
             >
-              Quiero ser voluntario
+              {i18nAbout('volunteerCta')}
             </button>
           </Link>
         </div>
@@ -234,21 +181,17 @@ export default function About() {
                 'main-red-button transition duration-300 hover:bg-RED_G20 w-[300px]'
               }
             >
-              Quiero ser Sponsor
+              {i18nAbout('donateHeader')}
             </Link>
           </div>
         </div>
         <div className={'w-5/6 md:w-1/3 order-1 md:order-none'}>
           <p className={'my-6 font-montserrat text-3xl'}>
-            Donaciones y sponsors
+            {i18nAbout('donateSubtitle')}
           </p>
           <h1 className={'mb-6'}>¡Ayúdanos a crecer!</h1>
           <p className={'leading-relaxed text-base'}>
-            NEXXOS cuenta con una amplia oferta de actividades académicas. Tu
-            contribución o sponsoreo nos ayuda a hacer más actividades y de
-            mejor calidad.
-            <br />
-            ¡Sumá tu aporte!
+            {i18nAbout('donateText')}
           </p>
         </div>
       </div>
